819be77ac00967e164990c925d8e990b2769d5ad
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2019-01-16 12:50:04 +0530

    Made html page and handle requests for stylesheet, images and homePage.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,30 @@
+const fs = require('fs');
+
 const app = (req, res) => {
-  res.statusCode = 404;
-  res.end();
+  const handledUrls = [
+    '/',
+    '/styleSheet.css',
+    '/freshorigins.jpg',
+    '/waterJar.gif'
+  ];
+  let page = './src/flowerCatalog.html';
+
+  if (req.url == '/styleSheet.css') {
+    page = './src/StyleSheet.css';
+  }
+
+  if (req.url == '/freshorigins.jpg') {
+    page = './images/freshorigins.jpg';
+  }
+
+  if (req.url == '/waterJar.gif') {
+    page = './images/waterJar.gif';
+  }
+
+  fs.readFile(page, (err, content) => {
+    res.write(content);
+    res.end();
+  });
 };
 
 // Export a function that can act as a handler
